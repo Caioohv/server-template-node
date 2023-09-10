cbb03ee98f4eb98da749f4cc50e2dc8aad0da1d1
Caio Vieira <caioviier@gmail.com>
2023-09-10 20:52:05 -0300

    feat: configuração básica do express

---
+++ config/express.cfg.js
@@ -0,0 +1,32 @@
+const morgan = require('morgan')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+
+morgan.token('statusColor', (req, res, args) => {
+  var status = (typeof res.headersSent !== 'boolean' ? Boolean(res.header) : res.headersSent)
+    ? res.statusCode
+    : undefined
+
+  var color = status >= 500 ? 31 // red
+    : status >= 400 ? 33 // yellow
+      : status >= 300 ? 36 // cyan
+        : status >= 200 ? 32 // green
+          : 0 // no color
+
+  return '\x1b[' + color + 'm' + status + '\x1b[0m'
+})
+
+module.exports = (app) => {
+
+  console.log('Booting Server...')
+
+  app.set('view cache', false)
+  app.use(bodyParser.json({limit: '20mb'}))
+  app.use(bodyParser.urlencoded({extended: true}))
+
+  app.use(morgan('\x1b[33m:method\x1b[0m \x1b[36m:url\x1b[0m :statusColor :response-time ms'))
+
+  app.use(cors())
+
+  console.log('Server ok')
+}
